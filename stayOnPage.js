3eec113e77cc01911d1e8700579d7b428d6c6355
jolanbosman <31825150+jolanbosman@users.noreply.github.com>
2018-05-22 09:43:29 +0200

    Delete stayOnPage.js

--- stayOnPage.js
+++
@@ -1,9 +0,0 @@
-// als op een link met een submenu geklikt wordt,
-// moet ik niet naar de koppeling gaan
-
-let heeftSub = document.querySelectorAll('.heeft-submenu > a');
-console.log(heeftsub);
-
-for(let i=0; i<heeftSub.length; i++) {
-  heeftSub[i].addEventListener('click', (e)=> e.preventDefault());
-}
